2be7bb9ee0be479dea2913f2ac138840c809bf32
alvaramz <adrian.alvarado_r@ucr.ac.cr>
2021-04-16 13:32:41 -0600

    Se agrega la estructura básica del proyecto

---
+++ FieldsCombination/src/main/java/xyz/facturacafe/fieldscombination/Principal.java
@@ -0,0 +1,18 @@
+package xyz.facturacafe.fieldscombination;
+
+import org.apache.logging.log4j.*;
+
+/**
+ * Clase contendora de la función main.
+ * @author Ing. Adrián Alvarado Ramírez.
+ */
+public class Principal {
+
+    private static final Logger log = LogManager.getLogger(Principal.class);
+
+    public static void main(String[] args){
+       log.info("Iniciando combinador de campos standalone");
+       
+    }
+
+}
